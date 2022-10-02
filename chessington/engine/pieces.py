d991eac48f5b05287c458a0316b683906f47b921
kriton <kriton_pilavidis@hotmail.com>
2022-10-02 10:14:37 +0100

    Final commit ....

--- chessington/engine/pieces.py
+++ chessington/engine/pieces.py
@@ -34,7 +34,44 @@ class Pawn(Piece):
     A class representing a chess pawn.
     """
     def get_available_moves(self, board) -> List[Square]:
-        return []
+        current_piece = board.find_piece(self)
+        avail_moves:List[Square] = []
+        if (self.player == Player.BLACK and current_piece.row - 2 > -1 ):
+
+            if(board.get_piece(Square.at(current_piece.row - 1  ,current_piece.col - 1)) and current_piece.col - 1 > -1 and
+               board.get_piece(Square.at(current_piece.row - 1  ,current_piece.col - 1)).player == Player.WHITE ):
+                avail_moves.append(Square.at(current_piece.row - 1  ,current_piece.col - 1))
+            
+            if (board.get_piece(Square.at(current_piece.row - 1  ,current_piece.col + 1)) and current_piece.col + 1 < 7 and
+               board.get_piece(Square.at(current_piece.row - 1  ,current_piece.col + 1)).player == Player.WHITE ):
+                avail_moves.append(Square.at(current_piece.row - 1  ,current_piece.col + 1))
+
+            if(not board.get_piece(Square.at(current_piece.row - 1  ,current_piece.col))):
+                avail_moves.append(Square.at(current_piece.row - 1  ,current_piece.col))
+            
+            if (current_piece.row == 6 and
+                not board.get_piece(Square.at(current_piece.row - 2  ,current_piece.col)) and
+                not board.get_piece(Square.at(current_piece.row - 1  ,current_piece.col))):
+                avail_moves.append(Square.at(current_piece.row - 2 ,current_piece.col))
+
+        elif(self.player == Player.WHITE and current_piece.row + 2 < 7 ):
+            if(board.get_piece(Square.at(current_piece.row + 1  ,current_piece.col - 1)) and current_piece.col - 1 > -1 and
+               board.get_piece(Square.at(current_piece.row + 1  ,current_piece.col - 1)).player == Player.BLACK ):
+                avail_moves.append(Square.at(current_piece.row + 1  ,current_piece.col - 1))
+            
+            if (board.get_piece(Square.at(current_piece.row + 1  ,current_piece.col + 1)) and current_piece.col + 1 < 7 and
+                board.get_piece(Square.at(current_piece.row + 1  ,current_piece.col + 1)).player == Player.BLACK ):
+                avail_moves.append(Square.at(current_piece.row + 1  ,current_piece.col + 1))
+            
+            if(not board.get_piece(Square.at(current_piece.row + 1  ,current_piece.col))):
+                avail_moves.append(Square.at(current_piece.row + 1,current_piece.col))
+
+            if (current_piece.row == 1 and current_piece.row + 2 < 8 and
+                not board.get_piece(Square.at(current_piece.row + 2  ,current_piece.col)) and
+                not board.get_piece(Square.at(current_piece.row + 1  ,current_piece.col))):
+                avail_moves.append(Square.at(current_piece.row + 2 ,current_piece.col))
+
+        return avail_moves
 
 
 class Knight(Piece):
@@ -79,4 +116,8 @@ class King(Piece):
     """
 
     def get_available_moves(self, board):
+        current_piece = board.find_piece(self)
+        avail_moves:List[Square]
+        
+
         return []
